1e9177ceacb25c3febb88895f13012309da17f9f
jacobtony <jacob.tony@brambles.com>
2024-10-16 23:49:44 +0530

    Updated App

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express();
 
 app.get( '/awsServices', ( req, res, next ) => {
-    res.json( [ 'ec2', 's3' ] )
+
+    console.log( 'awsServices requested' )
+    res.send( 'ec2s3' )
 } )
 console.log("Hello World")
 
